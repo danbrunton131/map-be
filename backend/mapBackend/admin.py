df626de5732e602bd237ab680ca53069ed89ce30
rishi41 <rishi41@users.noreply.github.com>
2020-04-30 18:40:41 -0400

    removed old stuff

--- backend/mapBackend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Course, Program, Requirement
-
-# Register your models here.
-admin.site.register(Course)
-admin.site.register(Program)
-admin.site.register(Requirement)
