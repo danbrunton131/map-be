9e8d719171f214dba87ef2514c78d1f96a4e8c84
Daniel <danbrunton131@gmail.com>
2022-03-09 22:45:22 -0500

    Filter out courses that aren't offered
    Filter out all the courses from the search that are not offered in any terms

--- backend/map_backend/views.py
+++ backend/map_backend/views.py
@@ -85,18 +85,27 @@ class SearchCourse(View):
         suggestions = []
 
         for course in res:
-            course_data = {
-                    "courseID": course.object.course_id,
-                    "courseCode": course.object.code,
-                    "courseName": course.object.name,
-                    "courseDesc": course.object.desc,
-                    "courseFall": course.object.offered_fall,
-                    "courseWinter": course.object.offered_winter,
-                    "courseSummer": course.object.offered_summer,
-                    "courseSpring": course.object.offered_spring
-            }
+            print('course search')
+            print(course.object.offered_fall)
+            
+            # only return courses that are offered during at least one term
+            if (course.object.offered_fall == True or
+                course.object.offered_winter == True or
+                course.object.offered_summer == True or
+                course.object.offered_spring == True):
+            
+                course_data = {
+                        "courseID": course.object.course_id,
+                        "courseCode": course.object.code,
+                        "courseName": course.object.name,
+                        "courseDesc": course.object.desc,
+                        "courseFall": course.object.offered_fall,
+                        "courseWinter": course.object.offered_winter,
+                        "courseSummer": course.object.offered_summer,
+                        "courseSpring": course.object.offered_spring
+                }
 
-            suggestions.append(course_data)
+                suggestions.append(course_data)
 
         response_data = {
                 'results' : suggestions # in list to maintain order/ranking
